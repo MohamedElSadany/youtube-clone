9600fde2830e4862efed175baa949c5ccb559069
mohamed.ElSadany <me2929977@gmail.com>
2023-03-11 16:43:30 +0200

    add project structure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# youtube-clone
+# Build and Deploy a Modern YouTube Clone Application in React JS with Material UI 5
+
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+
+
+const App = () => {
+ return(
+  <h1>hello</h1>
+ ) 
+}
+
+export default App;
